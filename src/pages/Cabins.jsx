1a71ce64f3374ad7fb4e1e15dea74eb2d2d2e996
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-10-08 19:56:11 +0530

    Integrated Supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { apiCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    apiCabins().then(function (data) {
+      console.log(data);
+    });
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
