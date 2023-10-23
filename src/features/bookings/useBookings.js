29471670872158ad7a4cf98ab9a915d2e1078134
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-10-23 11:37:57 +0530

    Prefecthing the booking cabin data

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import PAGE_SIZE from "../../utils/constants";
 
 export function useBookings() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // Filter
   const filterValue = searchParams.get("status");
@@ -24,6 +26,7 @@ export function useBookings() {
 
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // Query
   const {
     isLoading,
     data: { data, count } = {},
@@ -34,6 +37,28 @@ export function useBookings() {
       return getBookings({ filter, sortBy, page });
     },
   });
+
+  // Pre-Fetching
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: function () {
+        return getBookings({ filter, sortBy, page: page + 1 });
+      },
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: function () {
+        return getBookings({ filter, sortBy, page: page - 1 });
+      },
+    });
+  }
 
   return { isLoading, data, count };
 }
