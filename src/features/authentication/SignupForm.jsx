eb9b7876e427b16b7afd41d2b69b745616ea74f3
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-10-24 22:26:01 +0530

    Building sign up form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,27 +1,68 @@
+import styled from "styled-components";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useForm } from "react-hook-form";
 
 // Email regex: /\S+@\S+\.\S+/
 
+const Paragraph = styled.p`
+  color: red;
+  width: auto;
+  height: auto;
+`;
+
 function SignupForm() {
+  const { register, formState } = useForm();
+  const { errors } = formState;
   return (
     <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+      <FormRow>
+        <label>
+          <strong>Full Name</strong>
+        </label>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", { required: "This field is required" })}
+        />
+        <Paragraph>hi</Paragraph>
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow>
+        <label>
+          <strong>Email address</strong>
+        </label>
+        <Input
+          type="email"
+          id="email"
+          {...register("email", { required: "This field is required" })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow>
+        <label>
+          <strong>password (min 8 characters)</strong>
+        </label>
+        <Input
+          type="password"
+          id="password"
+          {...register("password", { required: "This field is required" })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow>
+        <label>
+          <strong>Repeat password</strong>
+        </label>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
